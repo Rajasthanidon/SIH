91773db2894eaa513efecfab337712341b92e208
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-09-10 01:13:34 +0530

    new8

--- Frontend/src/assets/login/Login.jsx
+++ Frontend/src/assets/login/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
         e.preventDefault();
 
         try {
-            const response = await fetch("/api/login", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
 
                 method: "POST",
                 headers: {
