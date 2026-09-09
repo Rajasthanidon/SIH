dcd557c01c1fbe8396d6e0779c3191463344a298
Rajasthanidon <nirdoshmeena350@gmail.com>
2026-09-10 01:16:26 +0530

    new8

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_API_URL}`,
+        target: "http://localhost:5000",
         changeOrigin: true,
 
       }
